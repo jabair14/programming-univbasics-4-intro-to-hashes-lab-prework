1e662964820baf0522aa802f7900a00d118a97ea
Jabair Khan <jabairk@gmail.com>
2021-02-01 19:50:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,8 +30,7 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  hash = {"name" => "Steve"}
-  hash["name"]
+  new_hash = {hash[key]}
   # return the correct value using the hash and key parameters
 end
 
