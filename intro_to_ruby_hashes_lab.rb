97de726fe31ac2fb3883c88d76bb26729be3160a
Jabair Khan <jabairk@gmail.com>
2021-02-01 20:10:58 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -35,7 +35,7 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  new_hash = hash[key]
+  #new_hash = hash[key]
   if hash[key] nil
     puts hash[key] += 1 
   # given a hash an a key as parameters, return an updated hash
