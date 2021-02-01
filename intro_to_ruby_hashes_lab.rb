746c35162dda3fcc988ed9042396626e25edfc34
Jabair Khan <jabairk@gmail.com>
2021-02-01 17:52:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,7 +24,7 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-        name = {"Grace Hopper" => 1}
+        name = {1 => "Grace Hopper"}
         name["Grace Hopper"]
   # return a hash that includes the key and value parameters passed into this method
 end
