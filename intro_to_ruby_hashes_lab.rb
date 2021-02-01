9201a5a553de3bd6a68e77b5711527e52bdde836
Jabair Khan <jabairk@gmail.com>
2021-02-01 19:23:16 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,8 +24,7 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-        if my_hash_creator = 'Grace Hopper' 
-          my_hash_creator[:name]
+        
             
   # return a hash that includes the key and value parameters passed into this method
 end
