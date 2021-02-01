6d2786eea68b8469fd1a39fa01c6ea7175ade0ba
Jabair Khan <jabairk@gmail.com>
2021-02-01 18:27:02 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,7 @@ def my_hash_creator(key, value)
         my_hash_creator = { 
           :name =>'Grace Hopper', :value => 1
         }
-        my_hash_creator[:name, :value]
+        my_hash_creator[:name]
   # return a hash that includes the key and value parameters passed into this method
 end
 
